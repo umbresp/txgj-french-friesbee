bdf9732a35743207e869ec1d13a781190cc307ef
Alexander Huang <huang@cs.utexas.edu>
2023-09-23 17:13:38 -0500

    flesh out dialogue capabilities

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 
@@ -58,6 +59,7 @@ public class DialogueManager : MonoBehaviour
 
         string sentence = sentences.Dequeue();
         curSentence = sentence;
+        // sentence = Regex.Replace(sentence, "<(?:\"[^\"]*\"['\"]*|'[^']*'['\"]*|[^'\">])+>", "");
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
@@ -66,15 +68,46 @@ public class DialogueManager : MonoBehaviour
     {
         isTyping = true;
         dialogueText.text = "";
-        foreach (char letter in sentence.ToCharArray())
+        for (int index = 0; index < sentence.Length; ++index)
         {
-            dialogueText.text += letter;
+            char letter = sentence[index];
+            if (letter == '<')
+            {
+                index = processTaggedLines(sentence, index);
+            }
+            else
+            {
+                dialogueText.text += letter;
+                // if (!textSound.isPlaying)
+                //     textSound.Play();
+            }
             if (dialogueText.text == sentence)
             {
                 isTyping = false;
             }
             yield return null;
         }
+    }
+
+    private int processTaggedLines(string sentence, int currIndex)
+    {
+        // Processes tags so that the tags themselves don't auto-fill with the scrolling text.
+        int frontTagEndIndex = sentence.IndexOf(">", currIndex) + 1;
+        string frontTag = sentence.Substring(currIndex, frontTagEndIndex - currIndex);
+
+        string backTag = frontTag[0] + "/" + frontTag.Substring(1);
+        string content = sentence.Substring(frontTagEndIndex, sentence.IndexOf(backTag, frontTagEndIndex) - frontTagEndIndex);
+
+        dialogueText.text += frontTag + backTag;
+        int newIndex = dialogueText.text.Length - backTag.Length;
+
+        int count = 0;
+        for (; count < content.Length; ++newIndex, ++count)
+        {
+            dialogueText.text = dialogueText.text.Insert(newIndex, content[count] + "");
+        }
+
+        return currIndex + frontTag.Length + content.Length + backTag.Length - 1;
     }
 
     void EndDialogue()
