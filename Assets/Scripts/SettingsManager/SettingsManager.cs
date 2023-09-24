6aa96454b42b13b234f8e4e9a45864fa87cee107
Austin Teng <austinhteng@gmail.com>
2023-09-24 10:36:20 -0500

    Screen entry animation timing

--- Assets/Scripts/SettingsManager/SettingsManager.cs
+++ Assets/Scripts/SettingsManager/SettingsManager.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 public class SettingsManager : MonoBehaviour
 {
     public Slider narratorSlider;
+    public RectTransform settingsPanel;
 
     public bool gambleToggled = true;
     public bool narratorToggled = true;
+
+    private bool setttingsArrive = false;
+    private bool settingsReady = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        settingsPanel.localPosition = new Vector3(0, 1150, 0);
+        bringInSettings();
     }
 
     // Update is called once per frame
@@ -27,5 +33,25 @@ public class SettingsManager : MonoBehaviour
 
     public void updateNarratorSetting() {
         narratorToggled = narratorSlider.value == 1;
+    }
+
+    public void bringInSettings() {
+        StopAllCoroutines();
+        StartCoroutine(SettingsPanelEnters());
+        setttingsArrive = true;
+    }
+
+    IEnumerator SettingsPanelEnters() {
+
+        while (Vector3.Distance(settingsPanel.localPosition, Vector3.zero) > 5) {
+            Vector3 interpPos = Vector3.Lerp(settingsPanel.localPosition, Vector3.zero, 0.05f);
+            settingsPanel.localPosition = interpPos;
+            
+            if (Vector3.Distance(settingsPanel.localPosition, Vector3.zero) <= 5) {
+                settingsPanel.localPosition = Vector3.zero;
+            }
+            yield return null;
+        }
+        settingsReady = true;
     }
 }
